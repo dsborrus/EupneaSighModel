d4b5dad5d3d85439c7d6daf6e7487386c1b63af8
dsborrus - home pc <dsborrus@gmail.com>
2020-04-30 15:15:32 -0400

    directory_fixes

--- Fig06EventTimingRelationshipCaVer03/do_make_pubfig.m
+++ Fig06EventTimingRelationshipCaVer03/do_make_pubfig.m
@@ -20,7 +20,7 @@ diary([name '/diaryfile'])
 
 % % parameters % %
 
-tmax = 5000;
+tmax = 30000;
 includec = 1;
 scale_calcium = 0.5;
 dosave = 0;
@@ -28,9 +28,9 @@ N = 80;
 
 amax=25;
 
-% control conditions
-jin0 = scale_calcium*0.019; % 0.02 0.018 0
-jin1=scale_calcium/amax * 0.1; % 0.02 0.04 0.2
+%% control conditions
+jin0 = scale_calcium*0.008; % 0.02 0.018 0
+jin1=scale_calcium/amax * 0.2; % 0.02 0.04 0.2
 
 [paramsC, outC] = tabakrinzelcalcium('ESCOUPLINGvsCa',1,...
                                    'ESCouplingFig',0,...
@@ -42,16 +42,71 @@ jin1=scale_calcium/amax * 0.1; % 0.02 0.04 0.2
                                    'fig',[0 0 0 0 0 0 0 0 0],...
                                    'dosave',dosave...
                                    );
-                               
 
+%% larger jin1 conditions (1.5x larger)
+jin1D1=jin1*(1.5); % 0.02 0.04 0.2
+
+[paramsD1, outD1] = tabakrinzelcalcium('ESCOUPLINGvsCa',1,...
+                                       'ESCouplingFig',0,...
+                                       'total',tmax,...
+                                       'includec',includec,...
+                                       'N',N,...
+                                       'jin0',jin0,...
+                                       'jin1',jin1D1,...
+                                       'fig',[0 0 0 0 0 0 0 0 0],...
+                                       'dosave',dosave...
+                                       );
+                                   
+%% smaller jin1 conditions (0.5x the size)
+jin1D2=jin1*(0.5); % 0.02 0.04 0.2
+
+[paramsD2, outD2] = tabakrinzelcalcium('ESCOUPLINGvsCa',1,...
+                                       'ESCouplingFig',0,...
+                                       'total',tmax,...
+                                       'includec',includec,...
+                                       'N',N,...
+                                       'jin0',jin0,...
+                                       'jin1',jin1D2,...
+                                       'fig',[0 0 0 0 0 0 0 0 0],...
+                                       'dosave',dosave...
+                                       );                                   
                                
-figure('position',[100 100 800 600])                               
-subplot(3,3,5);                               
+%% figure creation                               
                                
+figure('position',[1000 100 1500 1000])
+
+twin = 200;
+
+% control plot
+subplot(3,3,2); hold on;
+plot(outC.t,outC.a,'k');
+plot(outC.t(outC.Coupling.locs(outC.Coupling.sigh_i)),outC.Coupling.pks(outC.Coupling.sigh_i),'ro')
+plot(outC.t(outC.Coupling.locs(outC.Coupling.sigh_i-1)),outC.Coupling.pks(outC.Coupling.sigh_i-1),'bo')
+xlim([2000 2000+twin])
+subplot(3,3,5);                        
+histogram(outC.Coupling.EuSiIn)
+
+% larger jin1 plot
+subplot(3,3,3); hold on;
+plot(outD1.t,outD1.a,'k');
+plot(outD1.t(outD1.Coupling.locs(outD1.Coupling.sigh_i)),outD1.Coupling.pks(outD1.Coupling.sigh_i),'ro')
+plot(outD1.t(outD1.Coupling.locs(outD1.Coupling.sigh_i-1)),outD1.Coupling.pks(outD1.Coupling.sigh_i-1),'bo')
+xlim([2000 2000+twin])
+subplot(3,3,6);                        
+histogram(outD1.Coupling.EuSiIn)
+                               
+% smaller jin1 plot
+subplot(3,3,1); hold on;
+plot(outD2.t,outD2.a,'k');
+plot(outD2.t(outD2.Coupling.locs(outD2.Coupling.sigh_i)),outD2.Coupling.pks(outD2.Coupling.sigh_i),'ro')
+plot(outD2.t(outD2.Coupling.locs(outD2.Coupling.sigh_i-1)),outD2.Coupling.pks(outD2.Coupling.sigh_i-1),'bo')
+xlim([2000 2000+twin])
+subplot(3,3,4);                        
+histogram(outD2.Coupling.EuSiIn)
 %% Clean up 
 %save([sweepname '/matfile.mat'])
 t_taken = toc(mytic);
-if ispc; system(['move *.png ' name '/']); end   
+%if ispc; system(['move *.png ' name '/']); end   
 disp(['Time taken was ~' mat2str(round(t_taken,0)) ' seconds. Or ~' mat2str(round(t_taken/60,0)) ' minute.' ])
 disp('Exiting script. Have a nice day.')
 diary off
