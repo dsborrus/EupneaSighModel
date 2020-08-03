a1bdf7f77ef6a1d3cb7bfe9c77d4851a4f35a31f
dsborrus - home pc <dsborrus@gmail.com>
2020-08-03 15:12:09 -0400

    updating activity surface fiugre

--- Manuscript_Fig01/2020TabakRinzelSurfaceVer03/do_tabakrinzelsurface.m
+++ Manuscript_Fig01/2020TabakRinzelSurfaceVer03/do_tabakrinzelsurface.m
@@ -12,7 +12,7 @@ system('rm -r fig*')
     'trans',2000,'total',2025,'thintraj',0,'fig',[0 0 0 0 0 0 0 0 0 ],...
     'seed',0,'taua',0.001);
 
-pause(2);
+pause(1);
 
 [param, out_normal] = tabakrinzelcalcium('writetraj',1,'filenametraj','figdata1',...
     'includec',0,'includetheta',1,...
@@ -35,17 +35,40 @@ theta = 0.4:0.01:1; % 0.01
 %S = (4*(THETA+thetaa)-ka*log((1-A)./A))./(4*w*A); % using squash is better
 S = (4*(THETA+thetaa)-ka*A)./squash(A)/(4*w);
 
-% The plot itself
+% The activity plot itself
 surf(S,THETA,squash(A))
 xlabel('s'); ylabel('\theta');  zlabel('a')
 axis([ 0.4 1.1 -Inf Inf 0 1])
 shading interp
 alpha 0.7
-colormap cool
+colormap parula
 hold on;
 
-plot3(out_relax.s,out_relax.theta,out_relax.a,'-c')
-plot3(out_normal.s,out_normal.theta,out_normal.a,'-b','LineWidth',1)
+p1=1000;
+p2=1147;
+p3=2100;
+p4=4495;
+
+plot3(out_relax.s,out_relax.theta,out_relax.a,'-c');
+% placing directional arrows on the trajectory. This effect only works from
+% the default viewing angle!!!***
+plot3(out_relax.s(p1),out_relax.theta(p1),out_relax.a(p1),'c','marker','<','markerfacecolor','c')
+plot3(out_relax.s(p2),out_relax.theta(p2),out_relax.a(p2),'c','marker','v','markerfacecolor','c')
+plot3(out_relax.s(p3),out_relax.theta(p3),out_relax.a(p3),'c','marker','>','markerfacecolor','c')
+plot3(out_relax.s(p4),out_relax.theta(p4),out_relax.a(p4),'c','marker','^','markerfacecolor','c')
+
+p1=4210;
+p2=3999;
+p3=1800;
+p4=4495;
+
+plot3(out_normal.s,out_normal.theta,out_normal.a,'-b','LineWidth',1);
+% placing directional arrows on the trajectory. This effect only works from
+% the default viewing angle!!!***
+plot3(out_normal.s(p1),out_normal.theta(p1),out_normal.a(p1),'b','marker','>','markerfacecolor','b') %top left
+plot3(out_normal.s(p2),out_normal.theta(p2),out_normal.a(p2),'b','marker','v','markerfacecolor','b')
+plot3(out_normal.s(p3),out_normal.theta(p3),out_normal.a(p3),'b','marker','>','markerfacecolor','b') %bottom right
+plot3(out_normal.s(p4),out_normal.theta(p4),out_normal.a(p4),'b','marker','^','markerfacecolor','b')
 
 print('fig1.png','-dpng')
 print('fig1.pdf','-dpdf')
@@ -58,9 +81,6 @@ print('fig1.pdf','-dpdf')
 % fid = fopen('figdata2.dat','w');
 % fprintf(fid,'%6.8f  %6.8f  %6.8f\n',[squash(A1) THETA1 S1]');
 % fclose(fid);
-
-
-
 
 
 
