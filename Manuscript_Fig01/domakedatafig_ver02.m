840c2ad18e16ce61c3f2513b8177fe190eb59a3c
Daniel Borrus <dsborrus@gmail.com>
2020-08-19 12:06:28 -0400

    more work on fig1

--- Manuscript_Fig01/domakedatafig_ver02.m
+++ Manuscript_Fig01/domakedatafig_ver02.m
@@ -94,53 +94,27 @@ save('./tikz/data/data_D1.dat','data_D1','-ascii')
 save('./tikz/data/data_D2.dat','data_D2','-ascii')
 save('./tikz/data/data_D3.dat','data_D3','-ascii')
 
-% third column - bifurcation
-subplot(spr,spc,6); hold on;
+%% Third row
 
-%eq = load('../Fig03CalciumClosedCellModelVer02/MatCont7p2/Systems/sigh_bif_control/diagram/Step2_findequill_hopfs.mat');
-%eq_filler = load('../Fig03CalciumClosedCellModelVer02/MatCont7p2/Systems/sigh_bif_control/diagram/EP_EP(2).mat');
-%lc = load('../Fig03CalciumClosedCellModelVer02/MatCont7p2/Systems/sigh_bif_control/diagram/Step3_LimitCycle.mat');
+includec = 1;
+includes = 1;
+includetheta = 1;
+total = 2120;
+jin1 = 0.0020;
 
+[param_b,out_b] = tabakrinzelcalcium('includec',includec,...
+                                     'includes',includes,...
+                                     'includetheta',includetheta,...
+                                     'total',total,...
+                                     'jin1',jin1,...
+                                     'fig',fig);
+                                 
+subplot(3,3,7); hold on;
+plot(out_b.t,out_b.a)
 
-return
-
-% equill line
-j0_eq = eq.x(3,:);
-css = eq.x(1,:);
-
-hopfs = [16,39];
-
-% equll filler line
-j0_eqfill = eq_filler.x(3,:);
-css_eqfill = eq_filler.x(1,:);
-
-% limit cycle
-c_lc = lc.x(1:2:end-2,:);
-j0_lc = lc.x(324,:);
-period = lc.x(323,:);
-
-
-% figures
-plot(j0_eq(1:hopfs(1)),css(1:hopfs(1)),'k'); hold on;
-plot(j0_eq(hopfs(1):hopfs(2)),css(hopfs(1):hopfs(2)),'k--');
-plot(j0_eq(hopfs(2):end),css(hopfs(2):end),'k');
-plot(j0_eq(hopfs),css(hopfs),'bo')
-plot(j0_eqfill,css_eqfill,'k')
-xlim([0 0.2])
-% idk
-temp = 475;
-plot(j0_lc(1:temp),max(c_lc(:,1:temp)),'r');
-plot(j0_lc(1:temp),min(c_lc(:,1:temp)),'r');
-xlabel('j0')
-ylabel('c ss')
-% 
-% subplot(2,1,2)
-% plot(j0_lc(150:temp-42),period(150:temp-42),'linewidth',2)
-% xlim([0 0.2])
-% xlabel('jin0')
-% ylabel('period')
-
-% third simulation, both
+data_E1 = [out_b.t(1:sam:end)' out_b.a(1:sam:end)'];
+data_E2 = [out_b.t(1:sam:end)' out_b.s(1:sam:end)' out_b.theta(1:sam:end)' out_b.c(1:sam:end)' out_b.ct(1:sam:end)'];
+save('./tikz/data/data_E1.dat','data_E1','-ascii'); save('./tikz/data/data_E2.dat','data_E2','-ascii');
 
 function y = xinf(x,theta,k,lambda)
 y=lambda./(1+exp(4*(theta-x)/k));
